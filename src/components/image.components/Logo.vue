3ed5c09b2e476535cbf2d3a8362657d4b00161f9
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-02-25 03:28:15 +0100

    add header logo

---
+++ src/components/image.components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <img src="../../assets/img/netflux-logo.png" class="logo">
+</template>
+
+<script>
+
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style lang="scss">
+  .logo{
+    height: var(--fs-1);
+    padding-right: var(--m-5)
+  }
+</style>
