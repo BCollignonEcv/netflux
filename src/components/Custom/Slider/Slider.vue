6ddb6547b7ab21ca58c1746f3249c01b5b137ad7
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-03-15 03:25:23 +0100

    fix conflict

--- src/components/Custom/Slider/Slider.vue
+++ src/components/Custom/Slider/Slider.vue
@@ -17,13 +17,10 @@ export default {
     props: {
         title: String, 
         shows: Object,
-<<<<<<< HEAD
         wishlist: {
             type: Boolean,
             default: true
         },
-=======
->>>>>>> 78842f3439c1333d7af3fdab2f87118eedb6f9f6
     },
     setup(){
         return {
