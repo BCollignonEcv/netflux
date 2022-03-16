ecae962fa22001a7398cc472cd015cbf90bf7c61
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-03-16 01:19:35 +0100

    reorganise show comment

--- src/components/Custom/Show/index.js
+++ src/components/Custom/Show/index.js
@@ -1,13 +1,13 @@
 import ShowLanding from './ShowLanding.vue';
 import ShowComments from './ShowComments.vue';
-import ShowCommentsForm from './ShowComments.form.vue';
+import ShowCommentForm from './ShowComment.form.vue';
 import ShowEpisodes from './ShowEpisodes.vue';
 import ShowInfos from './ShowInfos.vue';
 
 export {
     ShowLanding,
     ShowComments,
-    ShowCommentsForm,
+    ShowCommentForm,
     ShowEpisodes,
     ShowInfos
 }
